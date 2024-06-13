09352d1e8169601488999ba7a80e6bdcd83336ee
Brian Maxwell Ketaren <86067355+brianketaren14@users.noreply.github.com>
2024-06-13 13:25:10 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import datetime
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+df_grouping = pd.read_csv('air_quality.csv')
+df_grouping['date'] = pd.to_datetime(df_grouping['date'])
+min_date = df_grouping['date'].min()
+max_date = df_grouping['date'].max()
+
+with st.sidebar:
+  st.image("https://github.com/dicodingacademy/assets/raw/main/logo.png")
+
+  start_date, end_date = st.date_input(
+      label='Rentang Waktu', min_value=min_date,
+      max_value=max_date,
+      value=[min_date, max_date]
+  )
+
+df_grouping = df_grouping[(df_grouping["date"] >= str(start_date)) & 
+                (df_grouping["date"] <= str(end_date))]
+
+df_Aotizhongxin_Changping_Dingling_Dongsi = df_grouping[(df_grouping['station'] == 'Aotizhongxin') | (df_grouping['station'] == 'Changping') | (df_grouping['station'] == 'Dingling') | (df_grouping['station'] == 'Dongsi')]
+df_Guanyuan_Gucheng_Huairou_Nongzhanguan = df_grouping[(df_grouping['station'] == 'Guanyuan') | (df_grouping['station'] == 'Gucheng') | (df_grouping['station'] == 'Huairou') | (df_grouping['station'] == 'Nongzhanguan')]
+df_Shunyi_Tiantan_Wanliu_Wanshouxigong = df_grouping[(df_grouping['station'] == 'Shunyi') | (df_grouping['station'] == 'Tiantan') | (df_grouping['station'] == 'Wanliu') | (df_grouping['station'] == 'Wanshouxigong')]
+
+
+def create_line_plot(df, column, title):
+    fig = plt.figure(figsize=(25,7))
+    sns.lineplot(data=df, x='date', y = column , hue='station')
+    plt.title(title, fontsize = 25)
+    plt.show()
+    st.pyplot(fig)
+
+col1, col2, col3, col4, col5 = st.columns(5) 
+with col1:
+  st.metric("Mean Of PM2.5", value=np.round(df_grouping['PM2.5'].mean()))
+  st.metric("Mean Of PM10", value=np.round(df_grouping['PM10'].mean()))
+
+with col2:
+  st.metric("Mean Of SO2", value=np.round(df_grouping['SO2'].mean()))
+  st.metric("Mean Of NO2", value=np.round(df_grouping['NO2'].mean()))
+
+with col3:
+  st.metric("Mean Of CO", value=np.round(df_grouping['CO'].mean()))
+  st.metric("Mean Of O3", value=np.round(df_grouping['O3'].mean()))
+
+with col4:
+  st.metric("Mean Of TEMP", value=np.round(df_grouping['TEMP'].mean()))
+  st.metric("Mean Of PRES", value=np.round(df_grouping['PRES'].mean()))
+
+with col5:
+  st.metric("Mean Of DEWP", value=np.round(df_grouping['DEWP'].mean()))
+  st.metric("Mean Of RAIN", value=np.round(df_grouping['RAIN'].mean()))
+
+st.subheader('PM2.5')
+create_line_plot(df_Aotizhongxin_Changping_Dingling_Dongsi, 'PM2.5', 'PM2.5 in Aotizhongxin_Changping_Dingling_Dongsi')
+create_line_plot(df_Guanyuan_Gucheng_Huairou_Nongzhanguan, 'PM2.5', 'PM2.5 in Guanyuan_Gucheng_Huairou_Nongzhanguan')
+create_line_plot(df_Shunyi_Tiantan_Wanliu_Wanshouxigong, 'PM2.5', 'PM2.5 in Shunyi_Tiantan_Wanliu_Wanshouxigong')
+
+st.subheader('TEMP')
+create_line_plot(df_Aotizhongxin_Changping_Dingling_Dongsi, 'TEMP', 'TEMP in Aotizhongxin_Changping_Dingling_Dongsi')
+create_line_plot(df_Guanyuan_Gucheng_Huairou_Nongzhanguan, 'TEMP', 'TEMP in Guanyuan_Gucheng_Huairou_Nongzhanguan')
+create_line_plot(df_Shunyi_Tiantan_Wanliu_Wanshouxigong, 'TEMP', 'TEMP in Shunyi_Tiantan_Wanliu_Wanshouxigong')
+
+st.write(df_grouping)
